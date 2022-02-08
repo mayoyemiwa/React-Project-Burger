171b7a624b9e5800dbc6a53af26a28ae56f48aaa
mayoyemiwa <mayoyemiwa@gmail.com>
2022-02-07 20:52:57 -0800

    Adding to package.json

--- userlogin-backend/server.js
+++ userlogin-backend/server.js
@@ -10,13 +10,13 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser())
 // app.use(express.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'build')));
 
-// -app.get('/', function (req, res) {
-// +app.get('/*', function (req, res) {
-//    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-//  })
-// })
+-app.get('/', function (req, res) {
++app.get('/*', function (req, res) {
+   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+ })
+})
 
 mongoose.connect(process.env.MONGODB_URI, 
   { useNewUrlParser: true, 
